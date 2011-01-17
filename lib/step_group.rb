a355df16871d6949274688683a9de075691bb10b
seancaffery <sean@lineonpoint.com>
2011-01-17 20:50:18 +1100

    added basic google chart generation for grouping

--- lib/step_group.rb
+++ lib/step_group.rb
@@ -1,3 +1,4 @@
+require 'cgi'
 class StepGroup
   attr_reader :id, :regex, :in_steps, :total_usage
 
@@ -29,6 +30,20 @@ class StepGroup
 
   def use_count
     @total_usage || 0
+  end
+
+  def group_graph
+    base = "https://chart.googleapis.com/chart?cht=gv:circo&chl=graph{"
+    base += "a [label=\"#{CGI.escape(CGI.escapeHTML(@regex.inspect.to_s))}\"];"
+
+    @in_steps.each do |id,in_step|
+
+      next if in_step[:step].regex.nil?
+      base += "a--\"#{CGI.escape(CGI.escapeHTML(in_step[:step].regex.inspect.to_s))}\" [weight=#{in_step[:count]}];"
+      #a [label=\"#{grouping.in_steps[0][:step].regex.inspect}\"]; a--b [penwidth=3,weight=2];b--d}"
+    end
+    base += "}"
+    base
   end
 
 end

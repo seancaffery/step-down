b3411febc6598334b7e63f7dd62b602f336c967a
seancaffery <sean@lineonpoint.com>
2011-03-02 20:42:45 +1100

    scenario spec

---
+++ spec/lib/scenario_spec.rb
@@ -0,0 +1,42 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../lib/scenario')
+require File.expand_path(File.dirname(__FILE__) + '/../../lib/step')
+
+
+describe Scenario do
+  before :each do
+    @scenario = Scenario.new
+    @s1 = Step.new(1, /step 1/)
+    @s2 = Step.new(2, /Step 2/)
+    @s2_dup = Step.new(2, /Step 2/)
+    @s3 = Step.new(3, /Step 3/)
+
+  end
+
+  describe "adding steps" do
+    it "should add steps to cache" do
+      @scenario.add_step(@s1)
+      @scenario.add_step(@s2)
+      @scenario.add_step(@s2_dup)
+      @scenario.add_step(@s3)
+
+      steps = [@s1, @s2, @s2_dup, @s3]
+      @scenario.steps.should =~ steps
+    end
+
+  end
+
+  describe "returning unique steps" do 
+    it "should only return one instance of each step" do
+      @scenario.add_step(@s1)
+      @scenario.add_step(@s2)
+      @scenario.add_step(@s2_dup)
+      @scenario.add_step(@s3)
+
+      steps = [@s1, @s2, @s3]
+      @scenario.uniq_steps.should =~ steps
+    end
+
+  end
+
+end
+

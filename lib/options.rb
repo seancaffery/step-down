0c3e2f2cb2ab76b88a51a2df71a9d524e852e0d9
seancaffery <sean@lineonpoint.com>
2011-03-07 19:46:34 +1100

    remove class variables

--- lib/options.rb
+++ lib/options.rb
@@ -1,28 +1,29 @@
 require 'optparse'
 
 class Options
+  attr_reader :steps_dir, :features_dir, :reporter
 
   OUTPUT_FORMATS = ["html", "text"]
 
   def parse(params)
-    @@steps_dir = "features/step_definitions"
-    @@features_dir = "features"
-    @@reporter = "html"
+    @steps_dir = "features/step_definitions"
+    @features_dir = "features"
+    @reporter = "html"
     parser = OptionParser.new do |opts|
       opts.banner = "Usage: stepdown step_definition_dir feature_file_directory"
 
       opts.separator("")
 
       opts.on("--output=TYPE", OUTPUT_FORMATS, "Select ouput format (#{OUTPUT_FORMATS.join(',')}). Default: html") do |o|
-        @@reporter = o
+        @reporter = o
       end
 
       opts.on("--steps=STEPS_DIR", "Step definition directory. Default: ./features/step_definitions") do |o|
-        @@steps_dir = o
+        @steps_dir = o
       end
 
       opts.on("--features=FEATURE_DIR", "Feature file directory. Default: ./features") do |o|
-        @@features_dir = o
+        @features_dir = o
       end
 
       opts.on_tail("-h", "--help", "You're looking at it") do |o|
@@ -42,10 +43,10 @@ class Options
   end
 
   def validate
-    @@steps_dir = File.join(Dir.pwd, @@steps_dir)
-    @@features_dir = File.join(Dir.pwd, @@features_dir)
+    @steps_dir = File.join(Dir.pwd, @steps_dir)
+    @features_dir = File.join(Dir.pwd, @features_dir)
 
-    [@@steps_dir, @@features_dir].each do |dir|
+    [@steps_dir, @features_dir].each do |dir|
       unless File.exists?(dir)
         puts "Directory #{dir} does not exist"
         exit 1
@@ -53,17 +54,4 @@ class Options
     end
 
   end
-
-  def steps_dir
-    @@steps_dir
-  end
-
-  def features_dir
-    @@features_dir
-  end
-
-  def reporter
-    @@reporter
-  end
-
 end

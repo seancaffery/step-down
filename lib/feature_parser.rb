99f8f0ed97c94971928ef65545170f424a6cca75
seancaffery <sean@lineonpoint.com>
2011-03-14 23:20:33 +1100

    little clean up

--- lib/feature_parser.rb
+++ lib/feature_parser.rb
@@ -1,27 +1,24 @@
-#!/usr/bin/ruby
-require 'rubygems'
-
-require File.expand_path(File.dirname(__FILE__) + '/step')
-require File.expand_path(File.dirname(__FILE__) + '/scenario')
+require 'step'
+require 'scenario'
 
 class FeatureParser
 
   def process_feature(file, instance)
-    @scenarios = []
+    scenarios = []
     file_lines = read_feature_file(file)
 
     file_lines.each do |line|
 
       if line =~ /Scenario|Background/
         @scenario = Scenario.new
-        @scenarios << @scenario
+        scenarios << @scenario
       else
         step = instance.line_matches(line)
         @scenario.add_step(step) if step
       end
     end
 
-    return @scenarios
+    scenarios
   end
 
 protected

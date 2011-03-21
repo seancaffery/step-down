0cfec3c1bcb127baae03f0e5d7502afd3e772aeb
seancaffery <sean@lineonpoint.com>
2011-03-21 20:19:40 +1100

    creating a Step object seems redundant

--- lib/step_collection.rb
+++ lib/step_collection.rb
@@ -8,18 +8,21 @@ class StepCollection
     @steps = {}
   end
 
-  def add_step(step)
-    if @steps[step.id]
-      @steps[step.id].count += 1
+  def add_step(id, regex)
+    if @steps[id]
+      @steps[id].count += 1
     else
-      @steps[step.id] = CountingStep.new(step.id, step.regex)
-      @steps[step.id].count = 1
+      @steps[id] = CountingStep.new(id, regex)
+      @steps[id].count = 1
     end
   end
-  alias_method :<<, :add_step
 
   def each
     @steps.each{|s| yield s[1] }
+  end
+
+  def length
+    @steps.length
   end
 
 end
--- spec/lib/step_collection_spec.rb
+++ spec/lib/step_collection_spec.rb
@@ -15,13 +15,13 @@ describe StepCollection do
     s1 = Step.new(1,/regex 1/)
     s2 = Step.new(2,/regex 2/)
     s3 = Step.new(3,/regex 3/)
-    @collection.add_step(s1)
-    @collection.add_step(s2)
-    @collection.add_step(s3)
+    @collection.add_step(1,/regex 1/)
+    @collection.add_step(2,/regex 2/)
+    @collection.add_step(3,/regex 3/)
 
-    @collection.steps[1].regex.should == s1.regex
-    @collection.steps[2].regex.should == s2.regex
-    @collection.steps[3].regex.should == s3.regex
+    @collection.steps[1].regex.should == /regex 1/
+    @collection.steps[2].regex.should == /regex 2/
+    @collection.steps[3].regex.should == /regex 3/
   end
 
   describe "adding a step to the step group" do
@@ -30,36 +30,31 @@ describe StepCollection do
     end
 
     it "should add new steps" do
-      step1 = Step.new(1,/regex/)
       counting_step = mock("counting_step")
       CountingStep.stub!(:new).and_return(counting_step)
       counting_step.should_receive(:count=).with(1)
-      @collection.add_step(step1)
+      @collection.add_step(1, /regex/)
       @collection.steps.should ==  {1 => counting_step}
 
     end
 
     it "should update the count for an existing step" do
 
-      step1 = Step.new(1,/regex/)
-      @collection.add_step(step1)
-      @collection.add_step(step1)
+      @collection.add_step(1,/regex/)
+      @collection.add_step(1,/regex/)
 
       @collection.steps[1].count.should == 2
     end
 
     it "should update step counts when multiple steps present" do
-      step1 = Step.new(1,/regex/)
-      @collection.add_step(step1)
-      @collection.add_step(step1)
+      @collection.add_step(1,/regex/)
+      @collection.add_step(1,/regex/)
 
-      step2 = Step.new(2,/regex/)
-      @collection.add_step(step2)
+      @collection.add_step(2,/regex/)
 
-      step3 = Step.new(3,/regex/)
-      @collection.add_step(step3)
-      @collection.add_step(step3)
-      @collection.add_step(step3)
+      @collection.add_step(3,/regex/)
+      @collection.add_step(3,/regex/)
+      @collection.add_step(3,/regex/)
 
       @collection.steps[1].count.should == 2
       @collection.steps[2].count.should == 1
